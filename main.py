ca03517699375392fb71f0b7c412e58c15bddcc9
KlaasMatlou <tshupianematlou@gmail.com>
2025-04-17 22:12:15 +0200

    Update and rename week1.py to main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from pet import Pet
+
+# Creating a Pet object named Thabo
+thabo = Pet("Thabo")
+
+# Now you can interact with the thabo object
+thabo.get_status()
+thabo.eat()
+thabo.play()
+thabo.get_status()
+thabo.train("roll over")
+thabo.show_tricks()
+thabo.train("fetch")
+thabo.show_tricks()
+thabo.train("roll over") # Try to teach an existing trick
+thabo.sleep()
+thabo.get_status()
